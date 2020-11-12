da77cd6f9d277024ccd95f26233a7b2f2d9ebafb
kunalver <kunalver213@gmail.com>
2020-11-12 22:04:30 +0530

    ini

--- src/main/java/com/kindsonthegenius/thymeleafapp/HomeController.java
+++ src/main/java/com/kindsonthegenius/thymeleafapp/HomeController.java
@@ -1,7 +1,18 @@
 package com.kindsonthegenius.thymeleafapp;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.client.ClientProtocolException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.rbi.errorhandler.RBIException;
+import com.rbi.pojo.AdvancesDetails;
+import com.rbi.pojo.Employee;
 
 @Controller
 public class HomeController {
@@ -9,6 +20,12 @@ public class HomeController {
 	@RequestMapping("/home")
 	public String test() {
 		return "index";
+	}
+	
+	@RequestMapping(value="/advdetails" , method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	public String getAdvDetails(){
+		
+		return "xxx";
 	}
 
 }
