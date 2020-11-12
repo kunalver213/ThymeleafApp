b3e05cfa701d34b63d2f9be5a06dd85f81ba1868
kunalver <kunalver213@gmail.com>
2020-11-12 22:21:47 +0530

    aa

--- src/main/java/com/kindsonthegenius/thymeleafapp/HomeController.java
+++ src/main/java/com/kindsonthegenius/thymeleafapp/HomeController.java
@@ -4,15 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.http.client.ClientProtocolException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.rbi.errorhandler.RBIException;
-import com.rbi.pojo.AdvancesDetails;
-import com.rbi.pojo.Employee;
 
 @Controller
 public class HomeController {
